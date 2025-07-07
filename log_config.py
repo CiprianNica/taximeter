be18a8e493fede575f3a93e5d5dee7ba39aa53b3
CiprianNica <ciprian.nica77@gmail.com>
2025-07-07 12:14:10 +0200

    feat: guarda trazas en archivos .logs diarios

--- log_config.py
+++ log_config.py
@@ -1,17 +1,22 @@
 import logging
 import logging.config
 import os
+from datetime import datetime
 
-# crear la carpeta de logs(si no existe)
-os.makedirs('logs', exist_ok=True)
 
 def config():
     '''
-    configuraciones trazas
+    configuraciones trazabilidad archivo y consola
     '''
+    # crear la carpeta de logs(si no existe)
+    os.makedirs('logs', exist_ok=True)
+    logs_archivo = f"logs/taximetro_{datetime.now().strftime('%d%m%Y')}.log"
+
     logging.basicConfig(
         level = logging.INFO,
-        filename = 'logs/taximetro.log',
-        filemode = 'a',
-        format = '%(asctime)s - %(levelname)s - %(message)s'
+        format = '%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(logs_archivo, mode='a', encoding='utf-8'),
+            logging.StreamHandler()
+        ]
     )
